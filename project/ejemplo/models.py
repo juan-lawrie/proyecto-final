b334e7917a54a5e950c42b085f38542c41825460
Juan Lawrie <juanlawrie7@gmail.com>
2022-12-05 17:01:35 -0300

    second commit

--- project/ejemplo/models.py
+++ project/ejemplo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Familiar(models.MoJuadel):
+class Familiar(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
--- project/ejemplo/views.py
+++ project/ejemplo/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
 
-    from django.shortcuts import render
-    from ejemplo.models import Familiar
-    def monstrar_familiares(request):
+def saludar_a(request,nombre):
+    return render(request, "ejemplo/saludar_a.html", {"nombre":nombre}
+)
+ 
+def monstrar_familiares(request):
         lista_familiares = Familiar.objects.all()
-    return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+        return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
+
+def sumar(request, a, b):
+    return render (request, "ejemplo/sumar.html", {"a": a,
+"b" : b,
+"resultado": a + b
+}
+)
+
+def buscar(request):
+    query = request.GET['q']
+    return render(request, "ejemplo/buscar.html", {"query": query})
