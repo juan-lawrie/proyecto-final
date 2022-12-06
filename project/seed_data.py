ee4528138ae34513f5b04a01908d056a4e3265da
Juan Lawrie <juanlawrie7@gmail.com>
2022-12-05 23:18:25 -0300

    fourth commit

--- project/seed_data.py
+++ project/seed_data.py
@@ -2,5 +2,5 @@ from ejemplo.models import Familiar
 Familiar(nombre="Marcos", direccion="9 de julio 850", numero_pasaporte=123123).save()
 Familiar(nombre="Pedro", direccion="San Martin 740", numero_pasaporte=890890).save()
 Familiar(nombre="Ale", direccion="25 de mayo 2650", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(nombre="Sole", direccion="Rio Parana 745", numero_pasaporte=567567).save()
 print("Se cargo con éxito los usuarios de pruebas")
